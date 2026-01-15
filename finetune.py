0ac68f16295b10f62b9da8f09368d0b7eeee03df
Ko0smos <164350270+Ko0smos@users.noreply.github.com>
2026-01-15 05:33:19 -0500

    Add finetune.py from bigcode-project/starcoder for learning

---
+++ finetune.py
@@ -0,0 +1,5 @@
+# This file was copied from bigcode-project/starcoder repository
+# Source: https://github.com/bigcode-project/starcoder/blob/main/finetune/finetune.py
+# For learning purposes about fine-tuning StarCoder models
+
+# Note: Please refer to the original repository for the complete and up-to-date version
